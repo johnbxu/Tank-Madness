8b6f8b2c9ef682b619e114cb4b32d33c5fd4c85d
John Xu <johnbxu@gmail.com>
2018-11-01 23:58:59 +0000

    added canvas and players

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom'
 import grass from './assets/grass.png'
 import './App.css';
-import Player from './Player'
-import random from './randomCoord'
 
 const KEY = {
   LEFT:  37,
@@ -17,6 +15,7 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
+      players: {},
       keys: {
         left  : 0,
         right : 0,
@@ -25,6 +24,7 @@ class App extends Component {
         space : 0,
       }
     }
+    this.socket = new WebSocket('ws://10.110.110.236:3003');
   }
 
   handleKeys(value, event) {
@@ -39,25 +39,16 @@ class App extends Component {
     });
   }
 
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeys.bind(this, true));
-    window.addEventListener('keyup',   this.handleKeys.bind(this, false));
 
+
+
+  componentDidMount() {
     const canvas = ReactDOM.findDOMNode(this.refs.canvas)
-    console.log(canvas)
     const ctx = canvas.getContext("2d")
+
     const img = new Image()
     img.src = grass
 
-    const player1 = new Player({
-      position: random()
-    })
-
-    const player2 = new Player({
-      position: random()
-    })
-
-    console.log(player1.position)
     img.onload = () => {
       const pattern = ctx.createPattern(img, 'repeat')
 
@@ -66,16 +57,81 @@ class App extends Component {
       ctx.fill()
 
       ctx.fillStyle = 'black'
-      ctx.fillRect(player1.position.x, player1.position.y, 30, 50)
-      ctx.fillRect(player2.position.x, player2.position.y, 30, 50)
+
     }
+    this.socket.onmessage = (event) => {
+      if (event.data === 'COLLISION!!!') {
+        this.setState({
+          collision: true
+        })
+      } else if (event.data === 'NO') {
+        this.setState({
+          collision: false
+        })
+      } else {
+        const state = JSON.parse(event.data)
+        const players = []
+        for (const player in state) {
+          players.push(state[player])
+        }
+        this.setState({
+          players: players
+        })
+
+        ctx.clearRect(0,0,1280,720)
+
+        const pattern = ctx.createPattern(img, 'repeat')
+
+        ctx.rect(0, 0, canvas.width, canvas.height)
+        ctx.fillStyle = pattern
+        ctx.fill()
+
+        ctx.fillStyle='blue'
+
+        for (let player of players) {
+          ctx.beginPath();
+          ctx.arc(player.position.x, player.position.y, 10, 0, 2 * Math.PI)
+          ctx.fill()
+          ctx.beginPath();
+          ctx.lineWidth = 5;
+          ctx.moveTo(player.position.x, player.position.y);
+          if (player.orientation === 'left') {
+            ctx.lineTo(player.position.x - 20, player.position.y)
+          }
+          if (player.orientation === 'right') {
+            ctx.lineTo(player.position.x + 20, player.position.y)
+          }
+          if (player.orientation === 'up') {
+            ctx.lineTo(player.position.x, player.position.y - 20)
+          }
+          if (player.orientation === 'down') {
+            ctx.lineTo(player.position.x, player.position.y + 20)
+          }
+          ctx.stroke()
+        }
+
+      }
+
+
+    }
+
+    window.addEventListener('keydown', this.handleKeys.bind(this, true));
+    window.addEventListener('keyup',   this.handleKeys.bind(this, false));
+
+    setInterval(() => {
+      this.socket.send(JSON.stringify(this.state.keys))
+    }, 1000/60)
+
+
   }
 
 
   render() {
+    const collision = this.state.collision && <h1>COLLISION!!!!!!!</h1>
     return(
       <div className="App">
         <canvas ref="canvas" width={1280} height={720} />
+        {collision}
       </div>
     )
   }
--- ws-server/server.js
+++ ws-server/server.js
@@ -1,10 +1,44 @@
+const uuid = require('uuid')
 // server.js
+const random = () => {
+  const coord = {}
+  coord.x = Math.random() * 1280
+  coord.y = Math.random() * 720
+  return coord
+}
+
+class Player {
+  constructor(id, position) {
+    this.id = id;
+    this.position = position;
+  }
+}
+const checkCollisionWith = (player, otherPlayers) => {
+  for (const ele in otherPlayers) {
+    if (player.id !== otherPlayers[ele].id) {
+      if (checkCollision(player, otherPlayers[ele])){
+        return true
+      }
+    }
+  }
+}
+
+const checkCollision = (obj1, obj2) => {
+  var vx = obj1.position.x - obj2.position.x;
+  var vy = obj1.position.y - obj2.position.y;
+  var length = Math.sqrt(vx * vx + vy * vy);
+  if (length < 20) {
+    console.log(length)
+    return true;
+  }
+  return false;
+}
 
 const express = require('express');
 const SocketServer = require('ws').Server;
 
-// Set the port to 3001
-const PORT = 3001;
+// Set the port to 3003
+const PORT = 3003;
 
 // Create a new express server
 const server = express()
@@ -12,15 +46,57 @@ const server = express()
   .use(express.static('public'))
   .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
 
+
 // Create the WebSockets server
 const wss = new SocketServer({ server });
 
+// Initializing the broadcast function
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    client.send(data);
+  });
+};
+
+const players = {};
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
 wss.on('connection', (ws) => {
   console.log('Client connected');
+  ws.id = uuid();
+  players[ws.id] = new Player(ws.id, random())
+  setInterval(function() {
+    wss.broadcast(JSON.stringify(players))
+  }, 1000/60)
+
+  ws.on('message', function incoming(data){
+    const movement = JSON.parse(data)
+    if (movement.left) {
+      players[ws.id].position.x -= 5
+      players[ws.id].orientation = 'left'
+    }
+    if (movement.right) {
+      players[ws.id].position.x += 5
+      players[ws.id].orientation = 'right'
+    }
+    if (movement.up) {
+      players[ws.id].position.y -= 5
+      players[ws.id].orientation = 'up'
+    }
+    if (movement.down) {
+      players[ws.id].position.y += 5
+      players[ws.id].orientation = 'down'
+    }
+    if (checkCollisionWith(players[ws.id], players)) {
+      wss.broadcast('COLLISION!!!')
+    } else {
+      wss.broadcast('NO')
+    }
+  })
 
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-  ws.on('close', () => console.log('Client disconnected'));
+  ws.on('close', () => {
+    delete players[ws.id]
+    console.log('Client disconnected');
+  })
 });
